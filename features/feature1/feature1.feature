5cdb995d84188ee2b6706318e9cd0bc06e4c68ce
udayswami <uday.swami@gslab.com>
2013-04-30 14:37:26 +0530

    added a gem

--- features/feature1/feature1.feature
+++ features/feature1/feature1.feature
@@ -1,6 +1,9 @@
 Feature: 'Contact Us link'
 
 Scenario:'When I go to home page I should have contact us link working'
-Given I go to home page
-Then I should see working contact us link
+Given I'm on houserie website
+When I click on login
+Then enter valid credentials
+And click on login
+Then user should be logged in successfully
 
--- features/feature1/step_definitions/feature1.rb
+++ features/feature1/step_definitions/feature1.rb
@@ -4,3 +4,27 @@ require "#{File.dirname(__FILE__)}/../../common_steps.rb" #include common steps 
 Then /^I should see working contact us link$/ do
   HomePage.contact_us
 end
+
+Given(/^I'm on houserie website$/) do
+  $browser.url.should include "http://staging.houserie.com/"
+end
+
+When(/^I click on login$/) do
+  $browser.link(:id,"login").click
+end
+
+Then(/^enter valid credentials$/) do
+  $browser.hidden.text_field(:xpath,"//*[@id=\"user_email\"]").set("hrentalroost@yahoo.com")
+#  $browser.text_field(:id,"user_password").set("hrental123")
+end
+
+Then(/^click on login$/) do
+  $browser.button(:id,"login_btn").click
+end
+
+Then(/^user should be logged in successfully$/) do
+  $browser.text.should include("Signed in successfully.")
+  $browser.div(:id,"user_name").text.should include "Hello Yorston"
+end
+
+
